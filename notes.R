838e8c0411ed066b14d09b1d497abaacc22d4a98
ahdvnd <hadavand.a@gmail.com>
2020-03-21 13:19:53 -0400

    final touches

---
+++ notes.R
@@ -0,0 +1,7 @@
+# google_form_decode("https://docs.google.com/spreadsheets/d/e/2PACX-1vQCAQ5AzDnc9p-4mk463_UEnL197cIBaTxPe-OxP88I3kxc2jqT29GzgEC3W0LkCysei2iZsxMDhBeM/pub?gid=1186532089&single=true&output=csv")
+
+
+# install_course_github("ahdvnd", "swirl-tbp", "cs112")
+
+uninstall_all_courses()
+install_course_github("ahdvnd", "cs112-swirl-courses")
