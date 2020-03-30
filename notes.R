7a2acc089a738b428e4b9f9f5bbd908056fca4c4
ahdvnd <hadavand.a@gmail.com>
2020-03-30 12:08:44 -0400

    final touches

--- notes.R
+++ notes.R
@@ -15,11 +15,34 @@ demo_lesson()
 
 
 
+# If you don't have the devtools packages
+# first install it!
+# install.packages("devtools")
+library(devtools)
+# This installs the modified swirl package from Github
+# Please do NOT install the swirl package by typing install.packages("swirl)
+# We are going to use a modified version of the package on my Github
+install_github("ahdvnd/swirlplus", force = TRUE)
+## you only have to perform the steps above once!
+
+
+
+## This you have to do everytime you open your RStudio to use swril
+library(swirl)
+# don’t need to do this step if doing this for the first time
+uninstall_all_courses()
+# this installs the lessons content
+install_course_github("ahdvnd", "cs112-swirl-courses")
+# if not working, restart your session
+# run the below command for starting swirl
+swirl()
+
+
 
 
 # install_course(swc_path = "/Users/Hadzzz/Google Drive/minerva_related/CS112/cs112-swirl-courses.swc")
 library(devtools)
-install_github("ahdvnd/swirlplus", force = TRUE)
+install_github("ahdvnd/swirlplus", force = TRUE, lib = Sys.getenv("R_LIBS_USER"))
 library(swirl)
 uninstall_all_courses()
 1
