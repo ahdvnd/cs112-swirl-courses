3a4370148c2ccedde4aa9395db58482551637239
ahdvnd <hadavand.a@gmail.com>
2020-08-31 12:09:21 -0400

    change notes.R file

--- notes.R
+++ notes.R
@@ -1,7 +1,7 @@
-
+# install.packages("swirl")
 # library(swirl)
 library(swirlify)
-demo_lesson()
+# demo_lesson()
 
 # devtools::install_github("jtleek/swirl")
 
@@ -13,7 +13,7 @@ demo_lesson()
 
 
 
-
+## Instruction for students
 
 # If you don't have the devtools packages
 # first install it!
@@ -39,33 +39,33 @@ swirl()
 
 
 
-
-# install_course(swc_path = "/Users/Hadzzz/Google Drive/minerva_related/CS112/cs112-swirl-courses.swc")
-library(devtools)
-install_github("ahdvnd/swirlplus", force = TRUE, lib = Sys.getenv("R_LIBS_USER"))
-library(swirl)
-uninstall_all_courses()
-1
-install_course_github("ahdvnd", "cs112-swirl-courses")
-# if not working, restart your session
-swirl()
-Aboozar
-1
-
-
-
-simple_reg <- glm(pbs2s3 ~ untype4 + wartype + logcost + wardur + factnum + factnum2 + trnsfcap + treaty + develop + exp + decade, family= binomial, data = peacekeeping)
-summary(simple_reg)
-genout <- GenMatch(X = X, Tr = peacekeeping$untype4)
-matchout.gen <- Match(X = X, Tr = peacekeeping$untype4, Y = peacekeeping$pbs2s3, Weight.matrix=genout)
-
-library(quantreg)
-rqfit <- rq(foodexp ~ income, tau = .5, data = engel)
-summary(rqfit)
-rq(foodexp ~ income, tau = , data = engel)
-rq(foodexp ~ income, tau = c(0.25, 0.50, 0.75), data = engel)
+# 
+# # install_course(swc_path = "/Users/Hadzzz/Google Drive/minerva_related/CS112/cs112-swirl-courses.swc")
+# library(devtools)
+# install_github("ahdvnd/swirlplus", force = TRUE, lib = Sys.getenv("R_LIBS_USER"))
+# library(swirl)
+# uninstall_all_courses()
+# 1
+# install_course_github("ahdvnd", "cs112-swirl-courses")
+# # if not working, restart your session
+# swirl()
+# Aboozar
+# 1
 
 
+
+# simple_reg <- glm(pbs2s3 ~ untype4 + wartype + logcost + wardur + factnum + factnum2 + trnsfcap + treaty + develop + exp + decade, family= binomial, data = peacekeeping)
+# summary(simple_reg)
+# genout <- GenMatch(X = X, Tr = peacekeeping$untype4)
+# matchout.gen <- Match(X = X, Tr = peacekeeping$untype4, Y = peacekeeping$pbs2s3, Weight.matrix=genout)
+# 
+# library(quantreg)
+# rqfit <- rq(foodexp ~ income, tau = .5, data = engel)
+# summary(rqfit)
+# rq(foodexp ~ income, tau = , data = engel)
+# rq(foodexp ~ income, tau = c(0.25, 0.50, 0.75), data = engel)
+# 
+# 
 
 
 
