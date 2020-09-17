54c0f4feb72ae2bfc8f224e80cdd0ce3db3d8712
ahdvnd <hadavand.a@gmail.com>
2020-09-16 21:23:44 -0400

    fix lesson 5

--- notes.R
+++ notes.R
@@ -23,7 +23,6 @@ install_github("ahdvnd/swirlplus", force = TRUE)
 ## you only have to perform the steps above once!
 
 
-
 ## This you have to do everytime you open your RStudio to use swirl
 library(swirl)
 # don’t need to do this step if doing this for the first time
@@ -34,37 +33,5 @@ install_course_github("ahdvnd", "cs112-swirl-courses")
 # if not working, restart your session
 # run the below command for starting swirl
 swirl()
-
-
-
-# 
-# # install_course(swc_path = "/Users/Hadzzz/Google Drive/minerva_related/CS112/cs112-swirl-courses.swc")
-# library(devtools)
-# install_github("ahdvnd/swirlplus", force = TRUE, lib = Sys.getenv("R_LIBS_USER"))
-# library(swirl)
-# uninstall_all_courses()
-# 1
-# install_course_github("ahdvnd", "cs112-swirl-courses")
-# # if not working, restart your session
-# swirl()
-# Aboozar
-# 1
-
-
-
-# simple_reg <- glm(pbs2s3 ~ untype4 + wartype + logcost + wardur + factnum + factnum2 + trnsfcap + treaty + develop + exp + decade, family= binomial, data = peacekeeping)
-# summary(simple_reg)
-# genout <- GenMatch(X = X, Tr = peacekeeping$untype4)
-# matchout.gen <- Match(X = X, Tr = peacekeeping$untype4, Y = peacekeeping$pbs2s3, Weight.matrix=genout)
-# 
-# library(quantreg)
-# rqfit <- rq(foodexp ~ income, tau = .5, data = engel)
-# summary(rqfit)
-# rq(foodexp ~ income, tau = , data = engel)
-# rq(foodexp ~ income, tau = c(0.25, 0.50, 0.75), data = engel)
-# 
-# 
-
-
 
 
