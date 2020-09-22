d5ed7347db1bcf41fd891de37f5e8ab6e34f46d1
ahdvnd <hadavand.a@gmail.com>
2020-09-22 08:59:54 -0400

    add lesson 8

---
+++ Session_8/initLesson.R
@@ -0,0 +1,11 @@
+# Code placed in this file fill be executed every time the
+      # lesson is started. Any variables created here will show up in
+      # the user's working directory and thus be accessible to them
+      # throughout the lesson.
+
+# swirl_options(swirl_logging = TRUE)
+
+install.packages("boot")
+library(Matching)
+library(boot)
+data(lalonde)
