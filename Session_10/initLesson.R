b9d6d904df2105df0911854ea2fbabf13dcfa1ee
Michael Chen <michaelchen@minerva.kgi.edu>
2020-09-23 21:53:08 +0100

    added lesson 10

---
+++ Session_10/initLesson.R
@@ -0,0 +1,19 @@
+# Code placed in this file fill be executed every time the
+      # lesson is started. Any variables created here will show up in
+      # the user's working directory and thus be accessible to them
+      # throughout the lesson.
+
+# swirl_options(swirl_logging = TRUE)
+
+library(Matching)
+data(lalonde)
+
+calc_rmse <- function(ytrue, ypred) {
+  res <- sum((ytrue - ypred)**2)
+  return(sqrt(res/length(ypred)))
+}
+
+set.seed(112)
+test <- sample(1:nrow(lalonde), 100)
+lalonde.test <- lalonde[test,]
+lalonde.train <- lalonde[-test,]
