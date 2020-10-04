636e3deaad1b455d6164219b2be2f725fc893a39
ahdvnd <hadavand.a@gmail.com>
2020-10-03 23:56:50 -0400

    add lesson 11

---
+++ Session_11/initLesson.R
@@ -0,0 +1,11 @@
+# Code placed in this file fill be executed every time the
+      # lesson is started. Any variables created here will show up in
+      # the user's working directory and thus be accessible to them
+      # throughout the lesson.
+
+# swirl_options(swirl_logging = TRUE)
+
+library(Matching)
+data(lalonde)
+
+set.seed(112)
