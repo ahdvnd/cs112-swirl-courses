281d410b5bf53d7868e02d9d3d34a56987a462ef
ahdvnd <hadavand.a@gmail.com>
2020-10-26 10:22:17 -0400

    fixed : issue

--- submission_analysis.R
+++ submission_analysis.R
@@ -7,7 +7,7 @@ library(dplyr)
 subs = read_sheet("1aUSIgj3KmHyiiz7H2CwOS4zp-NtDoaB4hihV0dbd05A") %>% 
     as.data.frame()
 
-colnames(subs) = c("timestamp", "name", "lesson", "secret", "section")
+colnames(subs) = c("timestamp", "name", "lesson", "secret", "section", "email")
 
 subs = subs %>% 
     mutate(valid = str_detect(secret, "CS112"))
@@ -19,9 +19,24 @@ subs_clean = subs %>%
     filter( valid == TRUE) %>% 
     group_by(secret) %>% 
     slice(1) %>% 
+    ungroup() %>%
+    group_by(name, lesson) %>% 
+    slice(1) %>% 
     ungroup() %>% 
     group_by(name) %>% 
-    summarise(total_subs = n())
+    summarise(total_subs = n()) %>% 
+    arrange(total_subs)
+
+# this is just to get emails
+emails = subs %>% 
+  filter(!is.na(email)) %>%
+  group_by(name) %>% 
+  slice(1)
+           
+# This is for Hiba for sending emails
+subs_clean_email = subs_clean %>% 
+  right_join(emails, by = "name") %>% 
+  select(name, total_subs, email)
 
 
 # The code below checks whether the person had entered the same
@@ -38,5 +53,4 @@ subs_check_duplicate = subs %>%
   group_by(name, secret) %>% 
   mutate(rp = n()) %>% 
   filter(rp>1)
-
   
