5cf4e28cd48aefe4b7aceb10875c5ee5013c7d95
Michael Chen <michaelchen@minerva.kgi.edu>
2020-10-29 10:50:38 +0000

    lesson 17 take 1

---
+++ Session_17/initLesson.R
@@ -0,0 +1,8 @@
+# Code placed in this file fill be executed every time the
+      # lesson is started. Any variables created here will show up in
+      # the user's working directory and thus be accessible to them
+      # throughout the lesson.
+
+# swirl_options(swirl_logging = TRUE)
+
+df = read.csv("https://drive.google.com/file/d/1l8k_2L4oPpf5Z_dFNCCjkYRBJmkQxMde/view")
