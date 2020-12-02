d19124e22a2aad0b6fb9a5aa840797557bb0ce4e
Michael Chen <michaelchen@minerva.kgi.edu>
2020-12-03 00:38:50 +0800

    lesson 24 troubleshoot

--- Session_24/subset.R
+++ Session_24/subset.R
@@ -1,11 +1,14 @@
-library(dplyr)
-female = data %>% 
-    as.data.frame() %>% 
-    filter(sex == 0)
+#library(dplyr)
+#female = data %>% 
+#    as.data.frame() %>% 
+#    filter(sex == 0)
     
-female_treat = female %>% 
-    filter(assignmt == 1)
+#female_treat = female %>% 
+#    filter(assignmt == 1)
 
-female_control = female %>% 
-    filter(assignmt == 0)
+#female_control = female %>% 
+#    filter(assignmt == 0)
 
+female = data[which(data$sex == 0),]
+female_treat = female[which(female$assignmt == 1),]
+female_control = female[which(female$assignmt == 0),]
