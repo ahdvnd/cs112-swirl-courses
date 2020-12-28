d5bd4cbdba21788d2a4a9e45b34701fb6ef2ad73
ahdvnd <hadavand.a@gmail.com>
2020-12-28 16:38:10 -0500

    fix urls in lessons 18 and 19

--- Session_17/initLesson.R
+++ Session_17/initLesson.R
@@ -5,3 +5,10 @@
 
 # swirl_options(swirl_logging = TRUE)
 
+.get_course_path <- function(){
+    tryCatch(swirl:::swirl_courses_dir(),
+             error = function(c) {file.path(find.package("swirl"),"Courses")}
+    )
+}
+
+lalonde_obs <- read.csv(file.path(.get_course_path(), "cs112-swirl-courses", "Session_17", "lalonde_obs.csv"))
